ba6b09c3601342f15869428fd2c18733bf1793d2
Destiny422 <161752238+Destiny422@users.noreply.github.com>
2025-11-05 12:55:17 -0400

    Update Reserva.java

--- src/modelo/Reserva.java
+++ src/modelo/Reserva.java
@@ -1,4 +1,55 @@
 package modelo;
-
+import java.util.Date;
 public class Reserva{
+  private String id;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private String estado;
+
+    public Reserva(String id, Date fechaInicio, Date fechaFin, String estado) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.estado = estado;
+    }
+
+    
+    public String toString() {
+        return "Reserva{" + "id=" + id + ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", estado=" + estado + '}';
+    }
+    public void mostrar(){
+        System.out.println(toString());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
