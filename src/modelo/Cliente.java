238608a889c019c46093dd824aa76c85f73342d9
Destiny422 <161752238+Destiny422@users.noreply.github.com>
2025-11-05 12:58:56 -0400

    Update Cliente.java

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,4 +1,66 @@
 package modelo;
 
 public class Cliente {
+  private String id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+
+    public Cliente(String id, String nombre, String apellido, String dni, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+    }
+
+   
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", telefono=" + telefono + '}';
+    }
+    public void mostrar(){
+        System.out.println(toString());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
 }
