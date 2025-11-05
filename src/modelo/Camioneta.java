5f10b9c43249d2883a0129bf320e89fdafe1a06f
Destiny422 <161752238+Destiny422@users.noreply.github.com>
2025-11-05 12:12:18 -0400

    Update Camioneta.java

--- src/modelo/Camioneta.java
+++ src/modelo/Camioneta.java
@@ -1,4 +1,35 @@
 package modelo;
 
 public class Camioneta {
+  private double capCarga;
+    private boolean dobleCabina;
+
+    
+    public String toString() {
+        return "Camioneta{" + "capCarga=" + capCarga + ", dobleCabina=" + dobleCabina + '}';
+    }
+
+    public Camioneta(double capCarga, boolean dobleCabina) {
+        this.capCarga = capCarga;
+        this.dobleCabina = dobleCabina;
+    }
+    public void mostrar(){
+        System.out.println(toString());
+    }
+
+    public double getCapCarga() {
+        return capCarga;
+    }
+
+    public void setCapCarga(double capCarga) {
+        this.capCarga = capCarga;
+    }
+
+    public boolean isDobleCabina() {
+        return dobleCabina;
+    }
+
+    public void setDobleCabina(boolean dobleCabina) {
+        this.dobleCabina = dobleCabina;
+    }
 }
