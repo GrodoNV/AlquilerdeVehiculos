080bdb44385a13185c4a6b62b8f98762b9ec3600
Destiny422 <161752238+Destiny422@users.noreply.github.com>
2025-11-05 12:36:35 -0400

    Update Inspeccion.java

--- src/modelo/Inspeccion.java
+++ src/modelo/Inspeccion.java
@@ -1,4 +1,55 @@
 package modelo;
-
+import java.util.Date;
 public class Inspeccion {
+  private String id;
+    private String tipo;
+    private Date fecha;
+    private String observaciones; 
+
+    public Inspeccion(String id, String tipo, Date fecha, String observaciones) {
+        this.id = id;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.observaciones = observaciones;
+    }
+
+    
+    public String toString() {
+        return "Inspeccion{" + "id=" + id + ", tipo=" + tipo + ", fecha=" + fecha + ", observaciones=" + observaciones + '}';
+    }
+    public void mostrar(){
+        System.out.println(toString());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
 }
