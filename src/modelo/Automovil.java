e2c73f4fdc7131e1639817c174f1f531bc35f267
Destiny422 <161752238+Destiny422@users.noreply.github.com>
2025-11-05 12:21:55 -0400

    Update Automovil.java

--- src/modelo/Automovil.java
+++ src/modelo/Automovil.java
@@ -1,4 +1,33 @@
 package modelo;
 
 public class Automovil {
+  private int numPuertas;
+    private String tipoCombi;
+
+    public Automovil(int numPuertas, String tipoCombi) {
+        this.numPuertas = numPuertas;
+        this.tipoCombi = tipoCombi;
+    }
+    public String toString() {
+        return "Automovil{" + "numPuertas=" + numPuertas + ", tipoCombi=" + tipoCombi + '}';
+    }
+    public void mostrar(){
+        System.out.println(toString());
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public String getTipoCombi() {
+        return tipoCombi;
+    }
+
+    public void setTipoCombi(String tipoCombi) {
+        this.tipoCombi = tipoCombi;
+    }
 }
